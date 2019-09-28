c20a20e0d3bee03a314d16a53885e9f9a2c5dae2
Ignacio Castañeda <icastanedaw@gmail.com>
2019-09-28 20:10:09 -0300

    Erased test folder

--- test_src/utils/hooks/index.js
+++
@@ -1,9 +0,0 @@
-export { useSelector } from "react-redux";
-export * from "./firebase";
-export * from "./springs";
-export { default as useForm } from "./useForm";
-export { default as useActions } from "./useActions";
-export { default as useClickEvent } from "./useClickEvent";
-export { default as useEdit } from "./useEdit";
-export * from "./useComponent";
-export * from "./useUsers";
